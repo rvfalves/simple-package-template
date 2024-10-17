6935be455674604cd6bab775e953f43ee41a9892
Rafael Alves <rvf_alves@yahoo.com.br>
2024-10-17 16:48:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="simple-bank-package",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Rafael Alves",
+    author_email="rvf_alves@yahoo.com.br",
+    description="Simple functions for bank operation",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="my_github_repository_project_link",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
